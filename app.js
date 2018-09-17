4e683e95599a31c29abb9e6b3777a0667127b1e6
xatier <xatierlike@gmail.com>
2018-09-16 19:20:05 -0700

    encode text correctly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ chrome.tabs.query({ currentWindow : true, highlighted : true }, function(tab) {
 
     url = 'https://twitter.com/intent/tweet?'
         + 'url=' + encodeURIComponent(url)
-        + '&text=' + encodeURI(title + '\n')
+        + '&text=' + encodeURIComponent(title + '\n')
 
     chrome.tabs.create({ url : url });
 });
