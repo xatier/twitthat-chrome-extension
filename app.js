980fe90a5baebe369930c263a8cc9a50dd653c4b
xatier <xatierlike@gmail.com>
2023-10-29 22:45:11 -0500

    feat: open new tab at next position to the current tab

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-chrome.tabs.query({ currentWindow : true, highlighted : true }, (tab) => {
-    let {url, title} = tab[0];
+chrome.tabs.query({ currentWindow: true, highlighted: true }, (tab) => {
+  let { url, title, index } = tab[0]
 
-    url = 'https://twitter.com/intent/tweet?'
-        + `url=${encodeURIComponent(url)}`
-        + `&text=${encodeURIComponent(title + "\n")}`
+  url =
+    "https://twitter.com/intent/tweet?" +
+    `url=${encodeURIComponent(url)}` +
+    `&text=${encodeURIComponent(title + "\n")}`
 
-    chrome.tabs.create({ url : url });
-});
+  chrome.tabs.create({ url: url, index: index + 1 })
+})
